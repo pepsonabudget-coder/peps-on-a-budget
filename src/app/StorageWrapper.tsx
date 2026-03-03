44a83861261c05f585cef67673dc0327827a1998
Martin <pepsonabudget@gmail.com>
2026-03-02 20:11:32 -0700

    Make storage boxes responsive on mobile

--- src/app/StorageWrapper.tsx
+++ src/app/StorageWrapper.tsx
@@ -5,6 +5,7 @@ import StorageBox from './products/storage/StorageBox'
 export default function StorageWrapper() {
   return (
     <div
+      className="storage-grid"
       style={{
         display: 'grid',
         gridTemplateColumns: 'repeat(4, 1fr)',
